917d83c02333cb18669c124bb6bc184cdec82822
hidayat <hidayatul.afriahman@tkdn.co.id>
2025-03-10 15:08:46 +0700

    perbaikan

--- server/api/merged-data.js
+++ server/api/merged-data.js
@@ -7,11 +7,11 @@ export default defineEventHandler(async (event) => {
     try {
         console.log("Connecting to databases...");
         // Query data dari database pertama dengan urutan berdasarkan created_at DESC
-        const [rows1] = await pool2.query("SELECT request_id, created_at, base_amount, payment_status FROM file_upload_detail ORDER BY created_at DESC");
+        const [rows1] = await pool2.execute("SELECT request_id, created_at, base_amount, payment_status FROM file_upload_detail ORDER BY created_at DESC");
         console.log("Fetched rows from db1:", rows1);
 
         // Query data dari database kedua dengan urutan berdasarkan tanggal DESC
-        const [rows2] = await pool.query("SELECT t_id, tanggal, tarif, status, user, pembayaran, lokasi, kendaraan FROM csv_transactions ORDER BY tanggal DESC");
+        const [rows2] = await pool.execute("SELECT t_id, tanggal, tarif, status, user, pembayaran, lokasi, kendaraan FROM csv_transactions ORDER BY tanggal DESC");
         console.log("Fetched rows from db2:", rows2);
 
         // Merge data dari kedua tabel
