f2b591e3b4bfdae2960a38771ae8975fe2ca86ba
envyst <nurardli.saputra@gmail.com>
2025-03-11 12:28:17 +0700

    update deploy strategy

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+git fetch
+git pull
+
+# Check if docker.env file exists
+if [ -f docker.env ]; then
+    echo "Sourcing environment file..."
+    declare -A env_vars
+    while IFS= read -r line || [ -n "$line" ]; do
+        # Ignore empty lines and comments
+        [[ -z "$line" || "$line" =~ ^# ]] && continue
+        
+        # Parse each line in the .env file
+        IFS='=' read -r key value <<< "$line"
+        env_vars["$key"]="$value"
+    done < docker.env
+
+    # Ensure required variables exist
+    if [[ -n "${env_vars[APP_NAME]}" && -n "${env_vars[TAG]}" ]]; then
+        export PROJECT="${env_vars[APP_NAME]}-${env_vars[TAG]}"
+        echo "PROJECT set to: $PROJECT"
+    else
+        echo "Error: APP_NAME or TAG not found in docker.env. Aborting..."
+        exit 1
+    fi
+else
+    echo "Error: environment file not found. Aborting..."
+    exit 1
+fi
+
+echo "Building & Running Image $PROJECT..."
+docker compose --env-file docker.env -p "$PROJECT" build --no-cache
+docker compose --env-file docker.env -p "$PROJECT" up -d --force-recreate
+
+# Check the exit code of the last command
+if [ $? -ne 0 ]; then
+    echo "Deploy failed!"
+    exit $?
+fi
+
+echo "Deploy completed."
+exit 0
